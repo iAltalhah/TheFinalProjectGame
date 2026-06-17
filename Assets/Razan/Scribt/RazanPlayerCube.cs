dab3b43c7fcce4913549cff722cfc5e09a1843e3
RazanFehaid0 <rrazannffh20@icloud.com>
2026-06-17 19:56:49 +0300

    //

--- Assets/Razan/Scribt/RazanPlayerCube.cs
+++ Assets/Razan/Scribt/RazanPlayerCube.cs
@@ -1,6 +1,34 @@
 using UnityEngine;
+using System.Collections;
 
-public class RazanPlayerCube
+public class RazanPlayerCube : MonoBehaviour
 {
-    
+
+    bool isFalling = false;
+    float downSpeed = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isFalling)
+        {
+            downSpeed += Time.deltaTime * 0.01f;
+            transform.position = new Vector3(transform.position.x, transform.position.y - downSpeed, transform.position.z);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            isFalling = true;
+            Destroy(gameObject, 10);
+        }
+    }
 }
