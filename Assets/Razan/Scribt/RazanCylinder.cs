4635027560a26ef6eeedf0fa6835dec2a1a958c5
RazanFehaid0 <rrazannffh20@icloud.com>
2026-06-30 17:47:21 +0300

    cli

--- Assets/Razan/Scribt/RazanCylinder.cs
+++ Assets/Razan/Scribt/RazanCylinder.cs
@@ -2,14 +2,13 @@ using UnityEngine;
 
 public class RazanCylinder : MonoBehaviour
 {
-  
     private void OnTriggerEnter(Collider other)
     {
-        
+        Debug.Log("Trigger entered by: " + other.name + " tag: " + other.tag);
+
         if (other.CompareTag("Player"))
         {
             Debug.Log("bbb");
         }
     }
 }
-
