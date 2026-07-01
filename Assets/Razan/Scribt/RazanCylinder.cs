68b3dfe958d1923727a054c1ebb8e2222e95d3e3
RazanFehaid0 <rrazannffh20@icloud.com>
2026-07-01 23:53:25 +0300

    script cylinder5

--- Assets/Razan/Scribt/RazanCylinder.cs
+++ Assets/Razan/Scribt/RazanCylinder.cs
@@ -1,7 +1,23 @@
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RazanCylinder : MonoBehaviour
 {
+   
+    public float speed = 2.0f;     
+    public float distance = 3.0f;  
+    private Vector3 startPosition;
+    void Start()
+    {
+        startPosition = transform.position;
+    }
+
+    void Update()
+    {
+        float movement = Mathf.Sin(Time.time * speed) * distance;
+        transform.position = startPosition + new Vector3(0, 0, movement);
+    }
+
     private void OnTriggerEnter(Collider other)
     {
         Debug.Log("Trigger entered by: " + other.name + " tag: " + other.tag);
